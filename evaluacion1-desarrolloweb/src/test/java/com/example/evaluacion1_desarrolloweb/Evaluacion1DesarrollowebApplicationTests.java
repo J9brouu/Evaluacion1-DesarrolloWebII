93ef7e4d194e91c595e17ce84cb479a0cf88ba22
Jonathan Aaron Ortiz Correa <jonathan.ortiz.correa@estudiante.ipss.cl>
2025-10-27 20:10:14 -0300

    Add files via upload

---
+++ evaluacion1-desarrolloweb/src/test/java/com/example/evaluacion1_desarrolloweb/Evaluacion1DesarrollowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evaluacion1_desarrolloweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion1DesarrollowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
